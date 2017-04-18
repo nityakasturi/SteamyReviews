56899e54240c9490747784e0bcad11771fbdd420
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-17 20:03:35 -0400

    Heroku circular dependency fix

--- app/steam/models/__init__.py
+++ app/steam/models/__init__.py
@@ -1,4 +1,4 @@
 from app import dynamodb
 from app.steam.models.review import Review
-from app.steam.models.tag import Tag
 from app.steam.models.game import Game
+from app.steam.models.tag import Tag
