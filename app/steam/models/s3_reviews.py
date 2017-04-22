37e4cdc4b91e1e4fa1839459bd5f0c5308abb8be
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-22 16:43:16 -0400

    Slight upgrade to the S3 task

--- app/steam/models/s3_reviews.py
+++ app/steam/models/s3_reviews.py
@@ -4,17 +4,19 @@ import json
 import os
 import requests
 import re
+import sys
 
-from app.steam.models import Review, Game, Tag
-from app.steam.util import data_file
+from app.steam.models import Review
 from app.steam.models.game import iter_all_games
-from app.steam.models.review import saved_review_generator
-from app.steam.models.tag import compute_reverse_index, create_tag_list
+from argparse import ArgumentParser
 from datetime import datetime
 from random import sample
 
-def get_missing_reviews_s3(games):
-    remaining = set(g.app_id for g in games)
+def get_missing_reviews_s3(games, instances, identifier):
+    already_computed = set(int(obj.key) for obj in Review.bucket.objects.all())
+    remaining = set(g.app_id
+                    for g in games
+                    if g.app_id % instances == identifier and g.app_id not in already_computed)
     while len(remaining) > 0:
         app_id, = sample(remaining, 1)
         if not Review.exists_in_s3(app_id):
@@ -26,4 +28,9 @@ def get_missing_reviews_s3(games):
             remaining.remove(app_id)
 
 if __name__ == '__main__':
-    get_missing_reviews_s3(list(iter_all_games()))
+    parser = ArgumentParser()
+    parser.add_argument("instances", help="Number of instances doing work.", type=int)
+    parser.add_argument("identifier", help="Instance number", type=int)
+    args = parser.parse_args()
+    get_missing_reviews_s3(list(iter_all_games()), args.instances, args.identifier)
+
