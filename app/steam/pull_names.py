26694874811b02e63545dd104cd5f88c36ed3fa5
Nitya Kasturi <nsk43@cornell.edu>
2017-04-24 23:48:16 -0400

    commiting so files can appear online

---
+++ app/steam/pull_names.py
@@ -0,0 +1,18 @@
+import numpy as np
+import json
+
+def matrix_to_names():
+    arr = np.load('data/compressed_matrix.npy')
+    app_ids = arr[:,0].astype(int)
+    with open('data/app_ids.json') as f:
+        app_id_to_name = json.load(f)
+    names = []
+    for game in app_id_to_name:
+        if int(game['app_id']) in app_ids:
+            names.append(game['name'].encode('ascii','ignore'))
+    with open('data/app_names.json', 'w') as f:
+        print json.dumps(names)
+        json.dump(names, f)
+
+if __name__ == '__main__':
+    matrix_to_names()
