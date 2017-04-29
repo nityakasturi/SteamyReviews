0783d45c8f3394ba67bc062cafb4f5c3a0426009
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-28 23:00:09 -0400

    Couple style changes

--- app/controllers/autocomplete_controller.py
+++ app/controllers/autocomplete_controller.py
@@ -12,9 +12,10 @@ from app.steam.models import Game
 def autocomplete():
     # Assign a member to the function itself (waaaaaaaatt???) because that template is essentially
     # static, but might change for different versions of the app, so it's not worth hard-coding
-    # anywhere
-    if not hasattr(autocomplete, "cache"):
+    # anywhere, but in development, it's still nice to see changes reflected, so it bypasses the
+    # cache.
+    if app.config["DEBUG"] or not hasattr(autocomplete, "cache"):
         app_id_to_name = [{"data": game.app_id, "value": game.name} for game in Game.get_all()]
         autocomplete.cache = render_template("autocomplete.js",
-                                              app_id_to_name=json.dumps(app_id_to_name))
+                                             app_id_to_name=json.dumps(app_id_to_name))
     return autocomplete.cache
--- app/templates/autocomplete.js
+++ app/templates/autocomplete.js
@@ -1,6 +1,7 @@
 $('#game-searchbar').autocomplete({
     lookup: {{ app_id_to_name | safe }},
     triggerSelectOnValidInput: false,
+    minChars: 2,
     onSelect: function(suggestion) {
         window.location = "/?app_id=" + suggestion.data
     }
