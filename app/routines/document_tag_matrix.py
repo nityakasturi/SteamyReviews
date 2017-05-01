45c610644839644fdcefeca202221f382a05384f
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-30 21:05:44 -0400

    Maybe we can use the Jaccard sim as a baseline
    Instead of ground truth queries, we can use the Jaccard sim to check the
    PR of various models.

---
+++ app/routines/document_tag_matrix.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import json
+import numpy as np
+
+from app.models import Game
+from app.models.tag import compute_reverse_index
+from app.utils import data_file
+
+if __name__ == '__main__':
+    games = list(Game.get_all())
+    reverse_index = compute_reverse_index(games)
+    doc_tag_matrix = np.zeros((len(games), len(reverse_index) + 1))
+    app_ids = sorted([game.app_id for game in games])
+    tag_ids = sorted(reverse_index.keys())
+    doc_tag_matrix[:, 0] = np.array(app_ids)
+
+    for app_index in xrange(len(games)):
+        for tag_index in xrange(len(reverse_index)):
+            if app_ids[app_index] in reverse_index[tag_ids[tag_index]]:
+                doc_tag_matrix[app_index, tag_index + 1] = 1
+
+    with open(data_file("doc_tag_matrix.npy"), "w") as f:
+        np.save(f, doc_tag_matrix)
