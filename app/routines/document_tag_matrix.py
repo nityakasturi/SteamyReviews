04dfb0c0fdbb20875fcfb0bd75112a36b28eda73
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-30 22:24:19 -0400

    Rocking and rolling

--- app/routines/document_tag_matrix.py
+++ app/routines/document_tag_matrix.py
@@ -10,7 +10,7 @@ from app.utils import data_file
 if __name__ == '__main__':
     games = list(Game.get_all())
     reverse_index = compute_reverse_index(games)
-    doc_tag_matrix = np.zeros((len(games), len(reverse_index) + 1))
+    doc_tag_matrix = np.zeros((len(games), len(reverse_index) + 1), dtype=np.int)
     app_ids = sorted([game.app_id for game in games])
     tag_ids = sorted(reverse_index.keys())
     doc_tag_matrix[:, 0] = np.array(app_ids)
@@ -20,5 +20,5 @@ if __name__ == '__main__':
             if app_ids[app_index] in reverse_index[tag_ids[tag_index]]:
                 doc_tag_matrix[app_index, tag_index + 1] = 1
 
-    with open(data_file("doc_tag_matrix.npy"), "w") as f:
+    with open(data_file("doc_tag_matrix.npy"), "wb") as f:
         np.save(f, doc_tag_matrix)
