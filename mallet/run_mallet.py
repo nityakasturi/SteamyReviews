514323a115a60b1d95d8d6b21fb78ba4194b898a
Paul Chesnais <paul.chesnais@gmail.com>
2017-05-01 23:25:58 -0400

    Precision recall (#16)
    * Starting work on some precision recall curves
    
    I think we really want to focus on getting some more ground truth
    queries. I used the dataset compiled by @nityak to train an LDA model
    using Mallet, then used the ranking produced as ground truth.
    
    * Maybe we can use the Jaccard sim as a baseline
    
    Instead of ground truth queries, we can use the Jaccard sim to check the
    PR of various models.
    
    * Rocking and rolling
    
    * Mallet models up your naughty place
    
    * Quick fix for run_mallet.py
    
    * Add iterations flag to run_mallet
    
    * Moar models
    
    * Even moar models! And fix the 5k dataset
    
    * More shitty models

--- mallet/run_mallet.py
+++ mallet/run_mallet.py
@@ -32,10 +32,11 @@ def import_data(data_dir, mallet_data, token_regex, **kwargs):
                             stdout=PIPE, shell=True)
     print_output(mallet_process)
 
-def train(input_file, model_path, num_topics, num_top_words, **kwargs):
+def train(input_file, model_path, num_topics, num_top_words, num_iterations, **kwargs):
     mallet_process = Popen(["mallet", "train-topics",
                             "--input", input_file,
                             "--num-topics", str(num_topics),
+                            "--num-iterations", str(num_iterations),
                             "--output-doc-topics", path.join(model_path, "doc_matrix.tsv"),
                             "--output-topic-keys", path.join(model_path, "feature_keywords.tsv"),
                             "--num-top-words", str(num_top_words)], stdout=PIPE, shell=True)
@@ -45,13 +46,15 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("model_name", help="Name for new model")
 
-    parser.add_argument("data_dir", type=str, nargs="?",
-                        help="Path to data. Defaults to `data/{model_name}`")
+    parser.add_argument("-d", "-data-dir", type=str, dest="data_dir",
+                        default=data_file("reviews_1000_tokens"),
+                        help="Path to data. Defaults to `data/reviews_1000_tokens`")
 
-    parser.add_argument("mallet_data", type=str, nargs="?",
-                        help="Path mallet data file. Defaults to `mallet/reviews.mallet")
+    parser.add_argument("-m", "-mallet-data", type=str, dest="mallet_data",
+                        default=mallet_file("reviews_1000.mallet"),
+                        help="Path mallet data file. Defaults to `mallet/reviews_1000.mallet")
 
-    parser.add_argument("model_path", type=str, nargs="?",
+    parser.add_argument("-p", "--model-path", dest="model_path", type=str, nargs="?",
                         help="Path to model directory. Defaults to `mallet/{model_name}`")
 
     parser.add_argument("-n", "--num-topics", dest="num_topics", type=int, default=40,
@@ -60,7 +63,10 @@ def main():
     parser.add_argument("-w", "--num-top-words", dest="num_top_words", type=int, default=20,
                         help="Number of words to output for each topics")
 
-    parser.add_argument("--token-regex", dest="token_regex", type=str, default="\\p{L}+",
+    parser.add_argument("-i", "--num-iterations", dest="num_iterations", type=int, default=1000,
+                        help="Number of Mallet iterations")
+
+    parser.add_argument("--token-regex", dest="token_regex", type=str, default="\\p{L}[\\p{L}\\p{P}]+\\p{L}",
                         help="Regex to use when tokenizing documents")
 
     args = parser.parse_args()
@@ -70,12 +76,6 @@ def main():
 
     if not path.exists(args.model_path):
         os.mkdir(args.model_path)
-
-    if args.data_dir is None:
-        args.data_dir = data_file("reviews_1000_tokens")
-
-    if args.mallet_data is None:
-        args.mallet_data = mallet_file("reviews.mallet")
 
     if not path.exists(args.mallet_data):
         import_data(**vars(args))
